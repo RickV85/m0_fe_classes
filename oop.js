2939104b4f2df3fb1dac800b278979f38faab68d
Rick Vermeil <rickv85@gmail.com>
2022-09-23 18:40:51 -0600

    Initial commit

--- oop.js
+++ oop.js
@@ -1,15 +1,53 @@
-// Write a class called Unicorn
-// it should have a dynamic name property
-// it should have a color property, that is silver by default
-// it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+// // Write a class called Unicorn
+// // it should have a dynamic name property
+// // it should have a color property, that is silver by default
+// // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn {
+  constructor(name){
+    this.name = name;
+    this.color = "silver";
+  }
+  say(name) {
+    this.name = `*~* ${name} *~*`;
+  }
+}
+var unicorn1 = new Unicorn("Sally");
+console.log(unicorn1);
 
+unicorn1.say("Sally");
+console.log(unicorn1);
 
+// // Write a class called Vampire
+// // it should have a dynamic name property
+// // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
+// // it should have a thirsty property, that is true by default
+// // it should have a drink method. When called, the thirsty property changes to false
+//
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    this.pet = "bat";
+    this.thirsty = true;
+  }
+  changePet(pet) {
+    this.pet = pet;
+  }
+  drink(thirsty) {
+    this.thirsty = false;
+  }
+}
 
-// Write a class called Vampire
-// it should have a dynamic name property
-// it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
-// it should have a thirsty property, that is true by default
-// it should have a drink method. When called, the thirsty property changes to false
+var dracula = new Vampire("Dracula");
+console.log(dracula);
+
+dracula.changePet("snake");
+console.log(dracula);
+
+var elanor = new Vampire("Elanor", "snake");
+console.log(elanor);
+
+elanor.drink();
+console.log(elanor);
 
 
 
@@ -21,6 +59,36 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+// Had to give up on this one but feel like I am close to figuring out how to make this eat method work!
+class Dragon {
+  constructor(name, rider, color, isHungry = true) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.hasEaten = 0;
+    this.isHungry = isHungry;
+  }
+
+  eat() {
+    this.hasEaten = this.hasEaten + 1;
+    if (this.hasEaten > 3) {
+      this.isHungry = false
+    }
+  }
+}
+
+var dragon1 = new Dragon("Sadie", "Joe", "Purple");
+console.log(dragon1);
+
+
+var dragon2 = new Dragon("Anastasia", "Dave", "pink");
+console.log(dragon2);
+
+dragon2.eat()
+dragon2.eat()
+dragon2.eat()
+dragon2.eat()
+console.log(dragon2)
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +99,47 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit {
+  constructor(name, dispo, age = 0, isAdult = false, isOld = false, hasRing = false) {
+    this.name = name;
+    this.dispo = dispo;
+    this.age = age;
+    this.isAdult = isAdult;
+    if (age >= 33) {
+      this.isAdult = true}
+    this.isOld = isOld;
+    if (age >= 101) {
+      this.isOld = true}
+    this.hasRing = hasRing;
+    if (name === "Frodo") {
+      this.hasRing = true
+    }
+  }
+  celebrateBirthday(age, isAdult, isOld) {
+    this.age = this.age +1;
+    this.isAdult = isAdult;
+    if (age >= 33) {
+      this.isAdult = true}
+    this.isOld = isOld;
+    if (age >= 101) {
+      this.isOld = true}
+  }
+}
+var hobbit1 = new Hobbit("Steve", "mad", 60);
+console.log(hobbit1);
+
+var hobbit2 = new Hobbit("Frodo", "happy", 25);
+console.log(hobbit2);
+
+var hobbit3 = new Hobbit("Gandolf", "serious", 347)
+console.log(hobbit3);
+
+var hobbit4 = new Hobbit("Jeff", "mean", 32)
+console.log(hobbit4);
+
+hobbit1.celebrateBirthday()
+console.log(hobbit1);
+
+hobbit4.celebrateBirthday()
+console.log(hobbit4);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+Yes, this has been a lot! I have had to learn a lot outside of the lessons as well. It was challenging!
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+Even with breaks, it is taxing to have a very active brain for 10 hours a day.
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+Not sure how to answer this but I am pretty fried. I hope that mod 0 will help me be as ready as I can be?
